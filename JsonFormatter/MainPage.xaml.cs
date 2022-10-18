5f6059aa6974301b88a0ab0056f29d9c1ea6e7b8
Luke Vo <contact@lukevo.com>
2022-10-18 20:18:13 +0700

    Added Save file example.

--- JsonFormatter/MainPage.xaml.cs
+++ JsonFormatter/MainPage.xaml.cs
@@ -1,11 +1,69 @@
-﻿namespace JsonFormatter;
+﻿using Android.Content;
+using Android.Webkit;
+using Java.Interop;
+using Java.IO;
+using Java.Net;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using System.Text;
+
+namespace JsonFormatter;
 
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
+
+		this.ModifyWebView();
+    }
+
+	void ModifyWebView()
+	{
+		WebViewHandler.Mapper.AppendToMapping("WebViewWithDownload", (handler, view) =>
+		{
+#if ANDROID
+			var andView = handler.PlatformView;
+			andView.SetDownloadListener(new DownloadListenerHandler());
+#endif
+		});
+	}
+
+	class DownloadListenerHandler : Java.Lang.Object, IDownloadListener
+	{
+		public async void OnDownloadStart(string? url, string? userAgent, string? contentDisposition, string? mimetype, long contentLength)
+		{
+            // There should be URL parsing for base64 as well but for now just get it
+            // For blob, extra parsing is needed
+            var content = url!;
+
+            var picker = new Intent(Intent.ActionCreateDocument);
+            picker.AddCategory(Intent.CategoryOpenable);
+            picker.SetType("application/json");
+            picker.PutExtra(Intent.ExtraTitle, "file.json"); // Or change file name to whatever
+
+            var (result, data) = await MauiProgram.RequestAndroidFileAsync(picker);
+            var fileUri = data?.Data;
+            if (result != Android.App.Result.Ok || fileUri is null)
+			{
+				// Handle error here
+				return;
+			}
+
+			var file = Platform.CurrentActivity?.ContentResolver?.OpenFileDescriptor(fileUri, "w");
+			if (file?.FileDescriptor is null)
+			{
+				// Something is wrong here
+				return;
+			}
+
+			using var jOutStream = new FileOutputStream(file.FileDescriptor);
+			var bytes = Encoding.UTF8.GetBytes(content);
+			jOutStream.Write(bytes);
+			await jOutStream.FlushAsync();
+			jOutStream.Close();
+        }
     }
 
 }
--- JsonFormatter/MauiProgram.cs
+++ JsonFormatter/MauiProgram.cs
@@ -1,7 +1,29 @@
-﻿namespace JsonFormatter;
+﻿using Android.App;
+using Android.Content;
+using Microsoft.Maui.LifecycleEvents;
+using System.Runtime.CompilerServices;
+
+namespace JsonFormatter;
 
 public static class MauiProgram
 {
+	public record ActivityResult(Result resultCode, Intent? data);
+
+	const int FileRequestCode = 100;
+	static TaskCompletionSource<ActivityResult>? fileTcs;
+
+	public static async Task<ActivityResult> RequestAndroidFileAsync(Intent intent)
+	{
+#if !ANDROID
+		throw new PlatformNotSupportedException("For Android only");
+#endif
+
+		fileTcs = new TaskCompletionSource<ActivityResult>();
+		Platform.CurrentActivity!.StartActivityForResult(intent, FileRequestCode);
+
+		return await fileTcs.Task;
+	}
+
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
@@ -11,6 +33,26 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+			.ConfigureLifecycleEvents(e =>
+			{
+				e.AddAndroid(droid =>
+				{
+					droid.OnActivityResult((activity, requestCode, resultCode, data) =>
+					{
+						if (requestCode == FileRequestCode)
+						{
+							var tcs = fileTcs;
+
+							if (tcs is not null)
+							{
+                                fileTcs = null;
+                                tcs?.TrySetResult(new(resultCode, data));
+                            }
+							
+						}
+					});
+				});
 			});
 
 		return builder.Build();
